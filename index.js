7e56b3e20ff704deab360e70874630074c1d43cc
Victor <frozen711.vr.vr@gmail.com>
2023-09-20 23:54:10 -0400

    fixed some errors in the index.js and added code in the generateMarkdown.js

--- index.js
+++ index.js
@@ -1,126 +1,97 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
-inquirer.propmt(
-    [
-        {
-            type:'input',
-            message: "What's the project title",
-            name: 'title',
-            //validate property to check that the user provides a value
-            validate: (value) =>{ if(value){return true} else {return 'Ineed a value to continue'}}
-        },
-        {
-            type: 'input',
-            message: "How do you install your app?",
-            name: 'installation',
-            //validate property to check that the user provides a value
-            validate: (value) =>{ if(value){return true} else {return 'Ineed a value to continue'}}
-        },
-        {
-            type: 'input',
-            message: "Instructions to be follow?",
-            name: 'instructions',
-            //validate property to check that the user provides a value
-            validate: (value) =>{ if(value){return true} else {return 'Ineed a value to continue'}}
-        },
-        {
-            type: 'input',
-            message: "Any credits?",
-            name: 'credit',
-            //validate property to check that the user provides a value
-            validate: (value) =>{ if(value){return true} else {return 'Ineed a value to continue'}}
-        },
-        {
-            type: 'input',
-            message: "How do you use your app?",
-            name: 'usage',
-            //validate property to check that the user provides a value
-            validate: (value) =>{ if(value){return true} else {return 'Ineed a value to continue'}}
-        },
-        {
-            //list of license
-            type: 'input',
-            message: "What license did you use?",
-            name: 'license',
-            choices: ['The MIT License', 'The GPL License', 'Apache License', 'GNU License', 'N/A'],
-            //validate property to check that the user provides a value
-            validate: (value) =>{ if(value){return true} else {return 'Ineed a value to continue'}}
-        },
-        {
-            type: 'input',
-            message: "GitHub Username:",
-            name: 'git',
-            //validate property to check that the user provides a value
-            validate: (value) =>{ if(value){return true} else {return 'Ineed a value to continue'}}
-        },
-        {
-            type: 'input',
-            message: "E-mail:",
-            name: 'email',
-            //validate property to check that the user provides a value
-            validate: (value) =>{ if(value){return true} else {return 'Ineed a value to continue'}}
-        },
-    ]
-).then{({
-    title,
-    installation,
-    instruction,
-    credits,
-    license,
-    git,
-    linkedin,
-    usage,
-    contribution
-})=>{
-    // template to be used
-    const template = `# ${title}
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [contribution](#contribution)
-    * [Credit](#credit)
-    * [License](#license)
-    * Installation
-    ${installation}
-    ##Usage
-    ${usage}
-    ##Contribution
-    ${contribution}
-    ### instruction
-    ${instruction}
-    ## Credit
-    ${credits}
-    ## License
-    ${license}
-
-    #Contact
-    *GitHub :${git}
-    *Linkedin :${linkedin}
-    *E-mail :${email}
-    `;
-    //function to create our readme using the fs
-    createNewfile(title,template);
-}
-};
-//creating our creatNewfile function
-function createNewfile(fileName,data){
-    //fs
-    fs.writeFile(`./${fileName.toLowerCase().split(' ').join('')}.md`, data,(err) => {
-        if(err){
-            console.log(err)
-        }
-        console.log('Your README has been generated');
-})
-}
-
+const questions =[
+    {
+        type:'input',
+        message: "What's the project title",
+        name: 'title',
+        //validate property to check that the user provides a value
+        validate: (value) =>{ if(value){return true} else {return 'I need a value to continue'}}
+    },
+    {
+        type: 'input',
+        message: "How do you install your app?",
+        name: 'installation',
+        //validate property to check that the user provides a value
+        validate: (value) =>{ if(value){return true} else {return 'I need a value to continue'}}
+    },
+    {
+        type: 'input',
+        message: "Instructions to be follow?",
+        name: 'instructions',
+        //validate property to check that the user provides a value
+        validate: (value) =>{ if(value){return true} else {return 'I need a value to continue'}}
+    },
+    {
+        type: 'input',
+        message: "Any credits?",
+        name: 'credit',
+        //validate property to check that the user provides a value
+        validate: (value) =>{ if(value){return true} else {return 'I need a value to continue'}}
+    },
+    {
+        type: 'input',
+        message: "How do you use your app?",
+        name: 'usage',
+        //validate property to check that the user provides a value
+        validate: (value) =>{ if(value){return true} else {return 'I need a value to continue'}}
+    },
+    {
+        //list of license
+        type: 'input',
+        message: "What license did you use?",
+        name: 'license',
+        choices: ['The MIT License', 'The GPL License', 'Apache License', 'GNU License', 'N/A'],
+        //validate property to check that the user provides a value
+        validate: (value) =>{ if(value){return true} else {return 'I need a value to continue'}}
+    },
+    {
+        type: 'input',
+        message: "GitHub Username:",
+        name: 'git',
+        //validate property to check that the user provides a value
+        validate: (value) =>{ if(value){return true} else {return 'I need a value to continue'}}
+    },
+    {
+        type: 'input',
+        message: "Linkedin Username:",
+        name: 'linkedin',
+        //validate property to check that the user provides a value
+        validate: (value) =>{ if(value){return true} else {return 'I need a value to continue'}}
+    },
+    {
+        type: 'input',
+        message: "E-mail:",
+        name: 'email',
+        //validate property to check that the user provides a value
+        validate: (value) =>{ if(value){return true} else {return 'I need a value to continue'}}
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+        //fs
+        fs.writeFile(fileName, data, function(err) {
+            console.log(fileName)
+            console.log(data)
+            if(err){
+                return console.log(err)
+            }
+            console.log('Your README has been generated');
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.propmt(questions)
+    .then(function(data) {
+        writeToFile("README.md", generateMarkdown(data));
+        console.log(data)
+    })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,73 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  
+  if (!license) {
+    return '';
+  }
+
+  switch(license) {
+    case 'MIT': 
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    case 'GPL':
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+    case 'Apache': 
+      return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    case 'GNU':
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+    default:
+      return '';  
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(!license) {
+    return '';
+  }
+
+  return `[License](${license})`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return ''; 
+  }
+
+  return `## License
+  
+  This project is licensed under the ${license} license.`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  https://github.com/${data.Username}/${data.Title}
+  ## Description
+  ${data.Description}
+  ## Table Of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions?](#questions?)
+  ## Installation
+  The following necessary dependencies must be installed to run the application, ${data.Installations}
+  ## Usage
+  In order to use this app, ${data.Usages}
+  ${renderLicenseSection(data.License)}
+  This project is licensed under the ${renderLicenseBadge()} license.
+  The license link can be found here ${renderLicenseLink}.
+  ## Contributing
+  Contibutors: ${data.Contribution}
+  ## Tests
+  The following is needed in order to run the test: ${data.Test}
+  ## Questions
+  If you have any questions about the repo or want to open an issue or contact ${data.Usages}
 
 `;
 }
